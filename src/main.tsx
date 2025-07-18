59e91fceee3f7357414d43065edf804d08ac8aa6
Nasib Babazade <143509503+Nasib-cmd@users.noreply.github.com>
2025-07-18 16:30:03 +0400

    configure tailwind v4 and shadcn integration

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
-
+import './index.css'
 import { routeTree } from "./routeTree.gen";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/integrations/query";
